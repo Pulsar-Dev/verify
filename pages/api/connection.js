ac026db30ad7d4b9ec8735522fb8b707c340f5aa
Lythium4848 <62567544+Lythium4848@users.noreply.github.com>
2023-02-04 20:57:55 +0000

    debug logs

--- pages/api/connection.js
+++ pages/api/connection.js
@@ -13,23 +13,25 @@ export default async function handler(req, res) {
         res.status(500).json({error: 'Missing code'})
         return
     }
-
+    console.log("a")
     const accessToken = await getAccessToken(code)
+    console.log("b")
+    console.log("TOKEN:", accessToken)
     const steamId = await getUserSteamID(accessToken)
-
+    console.log("c")
     const promise1 = Promise.all([
         getUserId(accessToken),
         getGmodstoreID(steamId)
     ])
-
+    console.log("d")
     const promise1Out = await promise1
     const userId = promise1Out[0]
     const gmodstoreId = promise1Out[1]
-
+    console.log("e")
     const gmodstorePurchases = await getGmodstorePurchases(gmodstoreId)
-
+    console.log("f")
     await givePulsarRoles(gmodstorePurchases, userId)
-
+    console.log("g")
     res.status(200).json({"data": "OK"})
 }
 
